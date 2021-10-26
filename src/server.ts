e9ed84ad4b7e12f535457851b1449cd5f51ac8eb
lajanzen <lara-janzen@hotmail.com>
2021-10-26 11:39:38 +0200

    Add route with parameters

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,16 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const users = ['Leon', 'Philipp', 'Marwin', 'Lara'];
+
+app.get('/api/users/:name', (req, res) => {
+  res.send(req.params.name);
+});
+
+app.get('/api/users', (_req, res) => {
+  res.send(users);
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
